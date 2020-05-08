f21581b255ba0f84f686f41bc77de74245898270
Lucas Unnerfelt <llasarus@gmail.com>
2020-05-08 13:29:20 +0200

    More instructions, additional Tikhonov inversion mode

--- invert.py
+++ invert.py
@@ -4,6 +4,9 @@ import numpy as np
 from load_image import load_image
 from misc import create_matrix
 
+import pdb
+import pyqtgraph as pg
+
 # Argument parsing.
 parser = argparse.ArgumentParser(description='Find fitness as function of C and p')
 parser.add_argument('-i', '--image', metavar='I', required=True, type=str, help='input images')
@@ -11,6 +14,7 @@ parser.add_argument('-g', '--greens', metavar='G', required=True, type=str, narg
 parser.add_argument('-o', '--output', metavar='O', required=True, type=str, help='output')
 parser.add_argument('-f', '--frames', metavar='N', required=True, type=int, nargs='+', help='input frames')
 parser.add_argument('-s', '--subpixels', metavar='XxYxWxH', required=True, type=str, help='Subpixels')
+parser.add_argument('-M', '--matrix', metavar='M', default='identity', type=str, help='Matrix to use in Tikhonov regularization. Values: identity (default), diff')
 
 parser.add_argument('-p', metavar='P', required=True, type=float, help='Momentum')
 parser.add_argument('-C', metavar='C', required=True, type=float, help='C parameter')
@@ -61,18 +65,45 @@ def invert_tikhonov(alpha, u, s, vt, images):
         imgs.append(b.reshape(80, 80)) # TODO: Fix this.
     return xs, imgs, msq
 
+def invert_tikhonov_diff(alpha, mat, images):
+    M = (np.eye(200) - np.eye(200, k=1))[:-1]
+    mat2 = np.vstack([mat, M * alpha])
+
+    xs = []
+    imgs = []
+    msq = 0
+
+    for image in images:
+        flat_image = np.ndarray.flatten(image)
+        target = np.hstack([flat_image, np.zeros(M.shape[0])])
+        x, res, _, _ = np.linalg.lstsq(mat2, target, rcond=None)
+        b = mat.dot(x)
+
+        msq = np.sum((flat_image - b) ** 2)
+        xs.append(x)
+        imgs.append(b.reshape(80, 80)) # TODO: Fix this.
+
+        msq += msq
+
+    return xs, imgs, msq
+
 # Now we try to find alpha as large as possible without sacrificing fitness.
 # This is done through a simple binary search.
 
+if args.matrix == "diff":
+    inv_func = lambda alpha: invert_tikhonov_diff(alpha, mat, images)
+else:
+    inv_func = lambda alpha: invert_tikhonov(alpha, u, s, vt, images)
+
 def evaluate(alpha):
-    xs, imgs, msq = invert_tikhonov(alpha, u, s, vt, images)
+    xs, imgs, msq = inv_func(alpha)
     return msq
 
 upper = 100
 lower = -100
 
-minimum = evaluate(10 ** lower)
-maximum = evaluate(10 ** upper)
+minimum = evaluate(10.0 ** lower)
+maximum = evaluate(10.0 ** upper)
 
 tol = 1e-4
 tol_it = 0.1
@@ -82,12 +113,12 @@ def is_good(alpha):
 
 while (upper - lower) > tol_it:
     mid = (upper + lower) / 2
-    if is_good(10 ** mid):
+    if is_good(10.0 ** mid):
         lower = mid
     else:
         upper = mid
 
-xs, imgs, msq = invert_tikhonov(10 ** lower, u, s, vt, images)
+xs, imgs, msq = inv_func(10.0 ** lower)
 
 # Save the results
 with h5py.File(args.output, 'w') as f:
